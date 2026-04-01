176266230eca6e3e5bef9f5615392ee4b4db08f4
Daniel Faria <danielcfaria@gmail.com>
2026-04-01 13:14:44 -0400

    Adicionando programas para o laboratorio

---
+++ cuiaba.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Definindo fuso horario para Cuiaba (MT)...
+
+tzutil /s "Central Brazilian Standard Time"
+
+echo Fuso horario configurado com sucesso!
+pause/media/daniel/LINUX MINT1/windows/labinfo_public_key.pem
+/media/daniel/LINUX MINT1/windows/rustdesk-1.4.6-x86_64.exe
+/media/daniel/LINUX MINT1/windows/senha.bat
+/media/daniel/LINUX MINT1/windows/setup.exe
+/media/daniel/LINUX MINT1/windows/veyon-4.10.2.0-win64-setup.exe
